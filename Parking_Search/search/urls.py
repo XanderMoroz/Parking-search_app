32ec4a2d809a2ce9c9b2c6df7e453571b45b1984
Александр <7176uiop3436>
2022-11-29 20:41:09 +0300

    final fixes

--- Parking_Search/search/urls.py
+++ Parking_Search/search/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import Search, AdrList
+from .views import Search
 
 urlpatterns = [
-    # Представляем наш класс списка объявлений(AdvertisementList из view.py) при помощи метода as_view.
-    path('', AdrList.as_view(), name='adresses_list'),
-    path('search', Search.as_view(), name='search_list'),
+    path('', Search.as_view(), name='search_list'),
 ]
--- Parking_Search/search/views.py
+++ Parking_Search/search/views.py
@@ -1,45 +1,20 @@
 from django.views.generic import ListView
-from .filters import ParkingFilter
 from .models import ParkingPlace
 # Create your views here.
 
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchHeadline
-from django.contrib.postgres.search import TrigramSimilarity
-# Create your views here.
-
-class AdrList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
-    model = ParkingPlace
-    # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'title'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
-    template_name = 'search/adr_list.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
-    context_object_name = 'products'
-
-    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
-        context = super().get_context_data(**kwargs)
-        qu = self.request.GET
-        qs = ParkingPlace.objects.annotate(search=SearchVector("title")).filter(search=SearchQuery(qu))
-        context['filter'] = ParkingFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
-        context['queryset'] = qs
-        return context
+# from django.contrib.postgres.search import TrigramSimilarity
 
 class Search(ListView):
     model = ParkingPlace
     template_name = 'search/search.html'
     context_object_name = 'search_list'
 
-    # Метод get_context_data позволяет нам изменить набор данных,
-    # который будет передан в шаблон.
+    # Метод get_context_data позволяет нам изменить набор данных, который будет передан в шаблон.
     def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
         # и вызываем у них метод get_context_data с теми же аргументами, что и были переданы нам.
-        # В ответе мы должны получить словарь.
         context = super().get_context_data(**kwargs)
-        # К словарю добавим текущую дату в ключ 'time_now'.
         qs = ParkingPlace.objects.all()
         query = self.request.GET.get("query")
         if query:
@@ -53,16 +28,7 @@ class Search(ListView):
                     stop_sel="</i></u></b>",
                 )
             )
-        # similar = ParkingPlace.objects.filter(
-        #         title__trigram_word_similar=query,
-        #     ).annotate(word_similarity=TrigramWordSimilarity(query, "title"),
-        #     ).values("title", "word_similarity").order_by("-word_similarity")
-        qst = ParkingPlace.objects.all()
-        similar = ParkingPlace.objects.filter(
-            title__trigram_similar=query,
-        ).annotate(similarity=TrigramSimilarity("title", query)).order_by("-similarity"),
         context['search_res'] = qs
-        context['search_sim'] = similar
         return context
 
 
